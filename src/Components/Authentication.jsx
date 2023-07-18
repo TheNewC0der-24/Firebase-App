a39f9d0c036ecc6a464ca342f627bdfa844981cc
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-07-19 04:45:24 +0530

    Firebase Authentication

--- src/Components/Authentication.jsx
+++ src/Components/Authentication.jsx
@@ -1,18 +1,26 @@
 import { useState } from 'react';
 
 import {
+    Box,
     Container,
     Input,
     InputGroup,
     InputRightElement,
     Button,
     Stack,
-    Box,
+    Alert,
+    AlertIcon,
+    AlertTitle,
+    AlertDescription,
 } from '@chakra-ui/react';
 
 import { app } from '../Config/FirebaseConfig';
 
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+
 const Authentication = () => {
+
+    let auth = getAuth();
 
     const [show, setShow] = useState(false);
     const [data, setData] = useState({});
@@ -24,12 +32,28 @@ const Authentication = () => {
         setData({ ...data, ...input });
     }
 
+    const handleSubmit = () => {
+        signInWithEmailAndPassword(auth, data.email, data.password)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                document.getElementById('alert').style.display = 'flex';
+                document.getElementById('alertMessage').innerHTML = error.message;
+            })
+    }
+
     return (
         <Container maxW="3xl">
+            <Alert status='error' style={{ display: "none", marginBottom: "2rem" }} id='alert'>
+                <AlertIcon />
+                <span id='alertMessage'></span>
+            </Alert>
             <Stack spacing={3}>
                 <Input
                     type='email'
                     placeholder='Enter email'
+                    focusBorderColor='teal'
                     name='email'
                     onChange={(event) => handleInput(event)}
                 />
@@ -39,6 +63,7 @@ const Authentication = () => {
                         pr='4.5rem'
                         type={show ? 'text' : 'password'}
                         placeholder='Enter password'
+                        focusBorderColor='teal'
                         name='password'
                         onChange={(event) => handleInput(event)}
                     />
@@ -50,7 +75,7 @@ const Authentication = () => {
                 </InputGroup>
             </Stack>
             <Box sx={{ mt: 3 }}>
-                <Button colorScheme='blue'>Submit</Button>
+                <Button colorScheme='teal' onClick={handleSubmit}>Submit</Button>
             </Box>
         </Container>
     )
