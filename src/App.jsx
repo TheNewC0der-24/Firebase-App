768f825a2dcf2d5b0d2c22817f3f723014e74cab
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-07-20 00:58:14 +0530

    Create Route for Login and Register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import './App.css';
+import {
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
 
-import { Heading } from '@chakra-ui/react'
-
-import Authentication from './Components/Authentication';
+import Login from './Components/Login';
+import Register from './Components/Register';
 
 function App() {
 
   return (
-    <div>
-      <Heading sx={{ textAlign: "center", margin: '5rem 0' }}>Firebase</Heading>
-      <Authentication />
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigate to="/login" replace />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+    </Routes>
   )
 }
 
-export default App
+export default App;
