37782f88ce4292564dfd1a64810c4a8d39b35acc
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-08-24 04:43:11 +0530

    ✨ feat: Add a new book

--- Library-Management/src/Components/AddBooks.jsx
+++ Library-Management/src/Components/AddBooks.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     Box,
     Card,
@@ -16,19 +16,58 @@ import {
 } from '@chakra-ui/react';
 
 import { IoLibrary } from 'react-icons/io5';
+import BookDataService from '../Service/BookServices';
 
 const AddBooks = () => {
 
     const [title, setTitle] = useState("");
     const [author, setAuthor] = useState("");
     const [status, setStatus] = useState("Available");
-    const [flag, setFlag] = useState(true);
     const [message, setMessage] = useState({ error: false, msg: "" });
+    const [showAlert, setShowAlert] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setMessage("");
+        if (title === "" || author === "") {
+            setMessage({ error: true, msg: "Please fill all the fields" });
+            return;
+        }
+
+        const newBook = {
+            title: title,
+            author: author,
+            status: status
+        }
+
+        try {
+            await BookDataService.addBooks(newBook);
+            setMessage({ error: false, msg: "Book added successfully" });
+        } catch (error) {
+            setMessage({ error: true, msg: error.message });
+        }
+
+        setTitle("");
+        setAuthor("");
+        setStatus("Available");
+    };
+
+    useEffect(() => {
+        if (message.msg) {
+            setShowAlert(true);
+            const timeout = setTimeout(() => {
+                setShowAlert(false);
+                setMessage({ error: false, msg: '' });
+            }, 3000);
+
+            return () => clearTimeout(timeout);
+        }
+    }, [message]);
 
     return (
         <React.Fragment>
-            <Box sx={{ margin: "2rem 0 1rem 0", height: "50px" }}>
-                {message?.msg && (
+            <Box sx={{ margin: "1rem 0 1rem 0", height: "50px" }}>
+                {showAlert && (
                     <Alert status={message?.error ? "error" : "success"}>
                         <AlertIcon />
                         <span>{message?.msg}</span>
@@ -44,21 +83,19 @@ const AddBooks = () => {
                 <Divider />
                 <CardBody>
                     <Stack spacing={3}>
-                        <Input type="text" placeholder="Book title" onChange={(e) => setTitle(e.target.value)} />
-                        <Input type="text" placeholder="Book author" onChange={(e) => setAuthor(e.target.value)} />
+                        <Input type="text" placeholder="Book title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                        <Input type="text" placeholder="Book author" value={author} onChange={(e) => setAuthor(e.target.value)} />
                         <ButtonGroup spacing='2'>
-                            <Button disabled={flag} colorScheme="blue" variant={flag ? "solid" : "outline"}
+                            <Button colorScheme="blue" variant={status === "Available" ? "solid" : "outline"}
                                 onClick={() => {
                                     setStatus("Available");
-                                    setFlag(true);
                                 }}
                             >
                                 Available
                             </Button>
-                            <Button disabled={!flag} colorScheme="blue" variant={!flag ? "solid" : "outline"}
+                            <Button colorScheme="blue" variant={status === "Unavailable" ? "solid" : "outline"}
                                 onClick={() => {
                                     setStatus("Unavailable");
-                                    setFlag(false);
                                 }}
                             >
                                 Unavailable
@@ -68,7 +105,7 @@ const AddBooks = () => {
                 </CardBody>
                 <Divider />
                 <CardFooter>
-                    <Button sx={{ width: "100%" }} type="Submit" colorScheme="blue" variant="solid">
+                    <Button onClick={handleSubmit} sx={{ width: "100%" }} type="Submit" colorScheme="blue" variant="solid">
                         Add Book
                     </Button>
                 </CardFooter>
