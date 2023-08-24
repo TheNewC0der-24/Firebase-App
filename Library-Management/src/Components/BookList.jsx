e9d5ca53198bdc90568ba6853aae9f26462fe559
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-08-25 04:28:04 +0530

    Fetch books and add delete book functionality

--- Library-Management/src/Components/BookList.jsx
+++ Library-Management/src/Components/BookList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     Box,
     Button,
@@ -10,18 +10,41 @@ import {
     Td,
     TableCaption,
     TableContainer,
+    Tag,
 } from '@chakra-ui/react';
 
 import { AiFillEdit } from 'react-icons/ai';
 import { FaTrash } from 'react-icons/fa';
 
-const BookList = () => {
+import BookDataService from '../Service/BookServices';
+
+const BookList = ({ getBookId }) => {
+
+    const [books, setBooks] = useState([]);
+
+    const getBooks = async () => {
+        const data = await BookDataService.getAllBooks();
+        setBooks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    useEffect(() => {
+        getBooks();
+    }, []);
+
+    const deleteHandle = async (id) => {
+        await BookDataService.deleteBook(id);
+        getBooks();
+    };
+
+    // const getBookId = async (id) => {
+    //     const data = await BookDataService.getBookById(id);
+    // };
+
     return (
         <Box sx={{ mb: 5 }}>
-            <Button>
+            <Button onClick={getBooks}>
                 Refresh List
             </Button>
-
             <TableContainer>
                 <Table variant="striped" colorScheme="blue" mt={5}>
                     <TableCaption>
@@ -36,19 +59,27 @@ const BookList = () => {
                         </Tr>
                     </Thead>
                     <Tbody>
-                        <Tr>
-                            <Td>Book Name</Td>
-                            <Td>Author</Td>
-                            <Td>ISBN</Td>
-                            <Td>
-                                <Button colorScheme="yellow" variant="solid" mr={3}>
-                                    <AiFillEdit style={{ marginRight: "0.2rem" }} /> Edit
-                                </Button>
-                                <Button colorScheme="red" variant="solid">
-                                    <FaTrash style={{ marginRight: "0.2rem" }} /> Delete
-                                </Button>
-                            </Td>
-                        </Tr>
+                        {
+                            books.map((book) => (
+                                <Tr key={book.id}>
+                                    <Td>{book.title}</Td>
+                                    <Td>{book.author}</Td>
+                                    <Td>
+                                        <Tag colorScheme={book.status === "Available" ? "green" : "red"}>
+                                            {book.status}
+                                        </Tag>
+                                    </Td>
+                                    <Td>
+                                        <Button colorScheme="yellow" variant="solid" mr={3} onClick={(e) => getBookId(book.id)}>
+                                            <AiFillEdit style={{ marginRight: "0.2rem" }} /> Edit
+                                        </Button>
+                                        <Button colorScheme="red" variant="solid" onClick={(e) => deleteHandle(book.id)}>
+                                            <FaTrash style={{ marginRight: "0.2rem" }} /> Delete
+                                        </Button>
+                                    </Td>
+                                </Tr>
+                            ))
+                        }
                     </Tbody>
                 </Table>
             </TableContainer>
