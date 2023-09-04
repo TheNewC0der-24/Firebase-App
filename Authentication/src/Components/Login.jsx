3adf92247210ba3bdfdda69495911e50dbe8914f
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-09-05 01:55:14 +0530

    Login

--- Authentication/src/Components/Login.jsx
+++ Authentication/src/Components/Login.jsx
@@ -21,9 +21,9 @@ import { HiOutlineMail } from 'react-icons/hi';
 import { RiLockPasswordLine } from 'react-icons/ri';
 import { FcGoogle } from 'react-icons/fc';
 
-import { app } from '../Config/FirebaseConfig';
+import { useAuth } from '../Context/AuthContext';
 
-import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const Login = () => {
 
@@ -32,24 +32,21 @@ const Login = () => {
 
     const navigate = useNavigate();
 
-    const [show, setShow] = useState(false);
-    const [data, setData] = useState({});
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const [showSuccessAlert, setShowSuccessAlert] = useState(false);
-    const [showErrorAlert, setShowErrorAlert] = useState(false);
+    const [show, setShow] = useState(false);
+
     const [errorMessage, setErrorMessage] = useState('');
+    const [showErrorAlert, setShowErrorAlert] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const handleClick = () => setShow(!show);
 
-    const handleInput = (event) => {
-        const input = { [event.target.name]: event.target.value };
-        setData({ ...data, ...input });
-    }
 
     const handleLoginWithGoogle = () => {
         signInWithPopup(auth, googleProvider)
             .then((response) => {
-                localStorage.setItem('email', JSON.stringify(response.user.email));
                 navigate('/home');
             })
             .catch((error) => {
@@ -62,39 +59,29 @@ const Login = () => {
             });
     }
 
-    const handleSubmit = () => {
-        signInWithEmailAndPassword(auth, data.email, data.password)
-            .then((response) => {
-                localStorage.setItem('access_token', JSON.stringify(response.user.accessToken));
-                localStorage.setItem('email', JSON.stringify(response.user.email));
-                setShowSuccessAlert(true);
+    const { login } = useAuth();
 
-                setTimeout(() => {
-                    setShowSuccessAlert(false);
-                    navigate('/home');
-                }, 1000);
-            })
-            .catch((error) => {
-                setShowErrorAlert(true);
-                setErrorMessage(error.message);
-
-                setTimeout(() => {
-                    setShowErrorAlert(false);
-                }, 3000);
-            });
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            setErrorMessage('');
+            setLoading(true);
+            await login(email, password);
+            navigate('/home');
+        } catch (error) {
+            setShowErrorAlert(true);
+            setErrorMessage(error.message);
+            setTimeout(() => {
+                setShowErrorAlert(false);
+            }, 3000);
+        }
+        setLoading(false);
     }
 
     return (
         <Container maxW="4xl">
 
             <Box sx={{ margin: "2rem 0", height: "50px" }}>
-                {showSuccessAlert && (
-                    <Alert status='success'>
-                        <AlertIcon />
-                        <span>Login successfully</span>
-                    </Alert>
-                )}
-
                 {showErrorAlert && (
                     <Alert status='error'>
                         <AlertIcon />
@@ -105,59 +92,66 @@ const Login = () => {
 
             <Box>
                 <SimpleGrid minChildWidth='250px' spacing='40px'>
-                    <Box p={3}>
-                        <Heading as="h3" color="teal" textAlign="center" sx={{ marginBottom: "2rem" }}>
-                            Sign in to Account
-                        </Heading>
+                    <form onSubmit={handleSubmit}>
+                        <Box p={3}>
+                            <Heading as="h3" color="teal" textAlign="center" sx={{ marginBottom: "2rem" }}>
+                                Sign in to Account
+                            </Heading>
 
-                        <Box sx={{ display: "flex", justifyContent: "center", mb: 5 }}>
-                            <Button onClick={handleLoginWithGoogle}>
-                                <FcGoogle size={25} style={{ marginRight: "0.5rem" }} />
-                                Sign in with Google
-                            </Button>
-                        </Box>
+                            <Box sx={{ display: "flex", justifyContent: "center", mb: 5 }}>
+                                <Button onClick={handleLoginWithGoogle}>
+                                    <FcGoogle size={25} style={{ marginRight: "0.5rem" }} />
+                                    Sign in with Google
+                                </Button>
+                            </Box>
 
-                        <Stack spacing={3}>
-                            <InputGroup>
-                                <InputLeftElement pointerEvents='none'>
-                                    <HiOutlineMail color='teal' />
-                                </InputLeftElement>
-                                <Input
-                                    type='email'
-                                    placeholder='Enter email'
-                                    focusBorderColor='teal'
-                                    name='email'
-                                    onChange={(event) => handleInput(event)}
-                                />
-                            </InputGroup>
-                            <InputGroup>
-                                <InputLeftElement pointerEvents='none'>
-                                    <RiLockPasswordLine color='teal' />
-                                </InputLeftElement>
-                                <Input
-                                    pr='4.5rem'
-                                    type={show ? 'text' : 'password'}
-                                    placeholder='Enter password'
-                                    focusBorderColor='teal'
-                                    name='password'
-                                    onChange={(event) => handleInput(event)}
-                                />
-                                <InputRightElement width='4.5rem'>
-                                    <Button h='1.75rem' size='sm' onClick={handleClick}>
-                                        {show ? 'Hide' : 'Show'}
-                                    </Button>
-                                </InputRightElement>
-                            </InputGroup>
-                        </Stack>
-                        <Box sx={{ display: "flex", justifyContent: "center", mt: 5 }}>
-                            <Button
-                                colorScheme='teal'
-                                onClick={handleSubmit}
-                            >
-                                Sign In
-                            </Button>
+                            <Stack spacing={3}>
+                                <InputGroup>
+                                    <InputLeftElement pointerEvents='none'>
+                                        <HiOutlineMail color='teal' />
+                                    </InputLeftElement>
+                                    <Input
+                                        type='email'
+                                        placeholder='Enter email'
+                                        focusBorderColor='teal'
+                                        name='email'
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        required
+                                    />
+                                </InputGroup>
+                                <InputGroup>
+                                    <InputLeftElement pointerEvents='none'>
+                                        <RiLockPasswordLine color='teal' />
+                                    </InputLeftElement>
+                                    <Input
+                                        pr='4.5rem'
+                                        type={show ? 'text' : 'password'}
+                                        placeholder='Enter password'
+                                        focusBorderColor='teal'
+                                        name='password'
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                    />
+                                    <InputRightElement width='4.5rem'>
+                                        <Button h='1.75rem' size='sm' onClick={handleClick}>
+                                            {show ? 'Hide' : 'Show'}
+                                        </Button>
+                                    </InputRightElement>
+                                </InputGroup>
+                            </Stack>
+                            <Box sx={{ display: "flex", justifyContent: "center", mt: 5 }}>
+                                <Button
+                                    colorScheme='teal'
+                                    type='submit'
+                                    isLoading={loading}
+                                >
+                                    Sign In
+                                </Button>
+                            </Box>
                         </Box>
-                    </Box>
+                    </form>
                     <Box bgColor="teal" sx={{ padding: "3rem" }}>
                         <Heading as="h3" color="white" textAlign="center">
                             Hello, Friend!
