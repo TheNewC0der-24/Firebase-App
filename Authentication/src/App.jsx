89599a7c073a8bbfc2aff1cf21b1f188bfca58ff
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-09-05 02:13:08 +0530

    Improve

--- Authentication/src/App.jsx
+++ Authentication/src/App.jsx
@@ -1,50 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  Navigate,
-  useLocation
-} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-import Login from './Components/Login';
 import Register from './Components/Register';
+import Login from './Components/Login';
 import Home from './Pages/Home';
-import LoadingScreen from './LoadingScreen/LoadingScreen';
+
+import { useAuth } from './Context/AuthContext';
 
 function App() {
 
-  const location = useLocation();
-
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, [location]);
+  const { currentUser } = useAuth();
 
   return (
-    <React.Fragment>
-      {
-        isLoading ? (
-          <LoadingScreen />
-        ) : (
-          <Routes>
-            <Route path='/' element={<Navigate to="/register" replace />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/home' element={
-              localStorage.getItem('access_token') || localStorage.getItem('email') !== null
-                ? <Home />
-                : <Navigate to="/login" replace />
-            } />
-          </Routes>
-        )
-      }
-    </React.Fragment>
+    <Routes>
+      <Route path='/' element={currentUser ? <Home /> : <Navigate to="/login" />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
   )
 }
 
